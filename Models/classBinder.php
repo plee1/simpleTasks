ea2e7f8a2efdd5e719a92dda924a512db2085254
danvasquez <dan@maestroinfo.com>
2013-07-23 20:51:06 -0400

    testing classbinder

--- Models/classBinder.php
+++ Models/classBinder.php
@@ -2,6 +2,13 @@
 
 trait simpleTasks_Models_classBinder{
 	protected function bindDataToClass($data){
+
+		//$properties = array_keys()
+
+		foreach($this as $key => $value){
+			echo "x: {$key} {$value}";
+		}
+
 		foreach($data as $val){
 			echo $val;
 		}
