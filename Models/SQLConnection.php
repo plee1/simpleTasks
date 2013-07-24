6a1c326ca0f832c7fbbc3d91fa4674cfc6191365
danvasquez <dan@maestroinfo.com>
2013-07-23 20:25:54 -0400

    add index

--- Models/SQLConnection.php
+++ Models/SQLConnection.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-class SQLConnection
+class SimpleTasks_Models_SQLConnection
 {
     public $sqlConnection = null;
     private $user = "root";
